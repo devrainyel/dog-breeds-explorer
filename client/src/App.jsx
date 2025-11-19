994d336b036c1eaaf990550eec8a9c4719f52087
Rainyel <devrainyel@gmail.com>
2025-11-20 00:52:36 +0800

    setup project

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import Layout from './components/layout/Layout'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
